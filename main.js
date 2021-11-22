c94a8fa970594a58ecaf2523c5c2a7d597c23151
johnaroj@hotmail.com <johnaroj@hotmail.com>
2021-11-22 06:39:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function updateList() {
+    const title = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
+        return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+    });
+
+    document.querySelectorAll('.selected-circle').forEach(c => c.classList.remove("selected-circle"));
+    document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(title[0])].classList.add("selected-circle");
+}
+
+updateList();
+window.addEventListener('scroll', updateList);
